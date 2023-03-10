0257589a12030e1e60007c23ba9203e877443a3c
pedroafonso2807 <pedro.marcato@sptech.school>
2023-03-10 18:51:06 -0300

    Tela login criada

---
+++ public/funcoes.js
@@ -0,0 +1,21 @@
+
+
+function perfil() {
+  telaLogin.style.display = "flex"
+  telaCadastro.style.display = "none"
+}
+
+function cadastreSe() {
+  telaCadastro.style.display = "flex"
+  telaLogin.style.display = "none"
+}
+
+function saiirX() {
+  telaLogin.style.display = "none"
+  telaCadastro.style.display = "none"
+}
+
+
+
+
+
