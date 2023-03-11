7a6f91b8d46e6dbc36eb08c5bcace94f3041da05
CaiqueGomesdaSilva <@caique.gsilva@sptech.school>
2023-03-10 21:37:19 -0300

    ajustes tela de login

--- public/funcoes.js
+++ public/funcoes.js
@@ -1,17 +1,24 @@
 
 
 function perfil() {
-  telaLogin.style.display = "flex"
+  backgroundBlack.style.display = "flex"
   telaCadastro.style.display = "none"
 }
 
 function cadastreSe() {
   telaCadastro.style.display = "flex"
-  telaLogin.style.display = "none"
+  backgroundBlack.style.display = "flex"
+  telaLogin.style.display = "none";
 }
 
-function saiirX() {
-  telaLogin.style.display = "none"
+function sairX() {
+  backgroundBlack.style.display = "none"
+  telaCadastro.style.display = "none"
+}
+
+function voltarLogin()  {
+
+  telaLogin.style.display = "flex"
   telaCadastro.style.display = "none"
 }
 
